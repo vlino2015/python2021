6ff1f65b3c9befe784f4e43824d98d6ab9b8b5f4
Veronica Lino <Veronica@digitalcrafts.com>
2021-09-08 14:42:56 -0500

    homeork

---
+++ homework/python101.py
@@ -0,0 +1,177 @@
+
+# strings 
+
+# "Veronica"
+
+
+# 'Lino'
+
+# "I'm a new developer"
+
+# 'I\'m a string and I have to escape my single quote.'
+
+
+# """We the People of the United States, in Order to form a 
+
+# more perfect Union, establish Justice, insure domestic 
+# Tranquility, provide for the common defense, promote the 
+# general Welfare, and secure the Blessings of Liberty to 
+# ourselves and our Posterity, do ordain and establish this 
+# Constitution for the United States of America."""
+
+
+# print('Hello' + 'world')  #Helloworld
+
+# print("3" + "5")  #35
+
+# print(3 + 5) # 8
+
+# print(3.14 * 2) # 6.28
+
+# True # 1 
+# False # 0
+
+# print(True + True)
+
+# print("""We the People of the United States, in Order to form a \v\v\v
+# \tmore perfect Union, establish Justice, insure domestic\n\n 
+# Tranquility, provide for the common defense, promote the 
+# general Welfare, and secure the Blessings of Liberty to 
+# ourselves and our Posterity, do ordain and establish this 
+# Constitution for the United States of America.""")
+
+# eight = 8
+
+# 8 + 8 
+
+# 8 - 2
+
+# 8.0/ 3.14
+
+
+# print(5 % 2)  #  2*2 +  1
+
+# print(2 ** 3) # 8
+
+
+# first_name = "Veronica"
+
+# last_name = "Lino"
+
+# print(last_name, first_name)  # Lino Veronica
+
+# print(first_name + " " + last_name) # Veronica    Lino 
+
+# print("Veronica Lino")
+
+
+# found_coins = 20 
+
+# magic_coins = 10    # 0010101110
+
+# stolen_coins = 3 
+
+# result = found_coins + magic_coins * 365 - stolen_coins * 52  #3514 
+
+# _class = "hello"
+
+# print(result)
+
+# first_name = "Kim"
+# last_name = "Kardashian"
+
+
+# Hello Matt Fisher, How are you doing today?
+
+# sentence = "Hello {1} {0} {0} {0}, how are you doing today?".format(first_name, last_name)
+
+# sentence = "Hello {first} {last} {last} {last}, how are you doing today?".format(first=first_name, last=last_name)
+
+# sentence2 = "Hello {} {}, how are you doing today?".format(first_name, last_name)
+# sentence3 = "Hello {} {}, how are you doing today?".format(first_name, last_name)
+
+# print(sentence)
+
+first_name = "Matt"
+last_name = "Fisher"
+
+
+sentence = f'Hello {first_name} {last_name} how are you doing today?'
+
+# print(sentence)
+
+
+# print( type(99.9) )
+
+
+
+# print( isinstance('99', int) )
+
+# print( abs(-5) )
+
+# 2 ** 2 
+
+# pow(2, 2)
+
+
+# print( round(5.5))
+
+
+# name = input("What is your name? ")
+
+# print(f'Your name is {name}')
+
+
+# print('hello world')
+
+# print('digital crafts')
+
+# age = 19
+# name = "Emily"
+# # 18, 19m 20
+# if(age >=21 ): 
+#     print('you can drink')
+# elif age < 18: 
+#     print("What are you trying to do?")
+# else: 
+#     print('you are too young to be here')
+
+# if age == 25:
+#     print(1)
+#     print(2)
+#     print(3)
+    
+#     if name == "Devin": 
+#         print('Emily is here')
+    
+# print('outside of my if statement')
+
+# caste
+# age = int(input('what is your age? '))
+
+# print(type(age))
+
+# new_age = age + 5
+
+# print(new_age)
+
+# counter = 0  # 0 - 10
+
+# while counter < 10: # 2 < 10
+#     print('Christine')
+#     counter = counter + 1 # counter = 1 +1 = 11
+    
+# print('outiside of while statement')
+
+# WHEN When wHen ===  when
+
+while True: 
+    answer = input('Say when: ')
+    
+    if answer.lower() == 'when': 
+        break
+    
+print('outside of of while loop')
+
+
+# Write an app which asks user for an input and then displays a message indicating whether the number is even or odd.dc
